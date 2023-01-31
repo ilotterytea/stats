d0a6b0ad4c71172f557b3a07b256c435ecf39406
ilotterytea <ilotterytea@proton.me>
2023-01-31 23:51:56 +0600

    launch the server

--- src/main/java/kz/ilotterytea/stats/Application.java
+++ src/main/java/kz/ilotterytea/stats/Application.java
@@ -14,5 +14,9 @@ public class Application {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+
+        Server server = new Server();
+        Runtime.getRuntime().addShutdownHook(new Thread(server::dispose));
+        server.init();
     }
 }
