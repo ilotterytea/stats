9a4a23680eb3d880fa42dd1d9a50a60fae809021
ilotterytea <ilotterytea@proton.me>
2023-04-06 22:35:31 +0400

    The utility for Hibernate

---
+++ src/main/java/kz/ilotterytea/stats/utils/HibernateUtil.java
@@ -0,0 +1,31 @@
+package kz.ilotterytea.stats.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ilotterytea
+ * @version 1.0
+ */
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        Logger log = LoggerFactory.getLogger(HibernateUtil.class.getSimpleName());
+        try {
+            return new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            log.error("Couldn't create a session: ", ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+
