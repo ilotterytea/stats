78d37b12459f1200571dca62adccfde25dd9992e
ilotterytea <ilotterytea@proton.me>
2023-04-10 13:30:55 +0400

    Added Twitch chat info collector

--- src/main/java/kz/ilotterytea/stats/Application.java
+++ src/main/java/kz/ilotterytea/stats/Application.java
@@ -3,6 +3,10 @@ package kz.ilotterytea.stats;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.*;
+import kz.ilotterytea.stats.entities.Channel;
+import kz.ilotterytea.stats.twitchbot.TwitchBot;
+import kz.ilotterytea.stats.utils.HibernateUtil;
+import org.hibernate.Session;
 
 @OpenAPIDefinition(
     info = @Info(
@@ -14,5 +18,8 @@ public class Application {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+
+        TwitchBot bot = new TwitchBot();
+        bot.run();
     }
 }
