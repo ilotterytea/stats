fdb6e6de71618c7e6853ea5e8c097c6b23ba79d9
ilotterytea <ilotterytea@proton.me>
2023-04-10 13:24:57 +0400

    Added consts for Twitch credentials

---
+++ src/main/java/kz/ilotterytea/stats/SharedConstants.java
@@ -0,0 +1,7 @@
+package kz.ilotterytea.stats;
+
+public class SharedConstants {
+    public static final String BOT_CLIENT_ID = System.getProperty("twitch_client_id");
+    public static final String BOT_ACCESS_TOKEN = System.getProperty("twitch_access_token");
+    public static final String BOT_OAUTH2_TOKEN = System.getProperty("twitch_oauth2_token");
+}
