478941fde90a383ed34a29a4f46f1d6a170f14ba
ilotterytea <ilotterytea@proton.me>
2023-12-13 21:52:31 +0600

    feat: twitch_irc example

--- twitch_bot/src/main.rs
+++ twitch_bot/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use twitch_irc::{
+    login::StaticLoginCredentials, ClientConfig, SecureTCPTransport, TwitchIRCClient,
+};
+
+#[tokio::main]
+pub async fn main() {
     println!("Hello, world!");
+
+    let config = ClientConfig::default();
+    let (mut incoming_messages, irc_client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    let irc_handle = tokio::spawn(async move {
+        while let Some(message) = incoming_messages.recv().await {
+            println!("received message: {:?}", message);
+        }
+    });
+
+    tokio::join!(irc_handle);
 }
