87f3b1b99546168f52ede238ee76874e2b321a18
ilotterytea <ilotterytea@proton.me>
2023-12-25 17:13:19 +0600

    feat: 7tv websocket lets gooooo

--- twitch_bot/src/main.rs
+++ twitch_bot/src/main.rs
@@ -1,11 +1,25 @@
+use std::sync::Arc;
+
+use common::{establish_connection, models::Channel};
+use tokio::sync::Mutex;
+use twitch_api::types::UserId;
 use twitch_irc::{
     login::StaticLoginCredentials, message::ServerMessage, ClientConfig, SecureTCPTransport,
     TwitchIRCClient,
 };
 
-use crate::handlers::handle_chat_messages;
+use crate::{
+    handlers::handle_chat_messages,
+    providers::{
+        seventv::{api::SevenTVAPIClient, SevenTVWebsocketClient},
+        WebsocketData,
+    },
+};
+
+use common::{schema::channels::dsl as ch, ExpressionMethods, QueryDsl, RunQueryDsl};
 
 mod handlers;
+mod providers;
 
 #[tokio::main]
 pub async fn main() {
@@ -14,6 +28,38 @@ pub async fn main() {
     let config = ClientConfig::default();
     let (mut incoming_messages, irc_client) =
         TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    let conn = &mut establish_connection();
+
+    let channel_ids: Vec<i32> = ch::channels
+        .filter(ch::opt_outed_at.is_null())
+        .select(ch::alias_id)
+        .get_results(conn)
+        .expect("Failed to get channel IDs");
+
+    let channel_ids = channel_ids
+        .iter()
+        .map(|x| UserId::new(x.to_string()))
+        .collect::<Vec<UserId>>();
+
+    let reqwest_client = reqwest::Client::default();
+
+    let seventv_api_client = Arc::new(SevenTVAPIClient::new(reqwest_client));
+
+    let seventv_websocket_data = Arc::new(Mutex::new({
+        let mut data = WebsocketData::default();
+        data.awaiting_channel_ids = channel_ids.clone();
+        data
+    }));
+
+    let mut seventv_websocket =
+        SevenTVWebsocketClient::new(seventv_websocket_data.clone(), seventv_api_client.clone())
+            .await
+            .unwrap();
+
+    let seventv_websocket_handle = tokio::spawn(async move {
+        seventv_websocket.run().await.unwrap();
+    });
 
     let irc_handle = tokio::spawn({
         async move {
@@ -27,5 +73,5 @@ pub async fn main() {
         }
     });
 
-    tokio::join!(irc_handle);
+    tokio::join!(irc_handle, seventv_websocket_handle);
 }
