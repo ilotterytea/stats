aca687aaf98e6cd175954747a0333fbe9380658c
ilotterytea <ilotterytea@proton.me>
2023-12-26 21:20:25 +0600

    feat: get channel emotes by twitch id route

--- api/src/main.rs
+++ api/src/main.rs
@@ -13,11 +13,21 @@ async fn main() -> Result<()> {
     println!("Hello, world!");
 
     HttpServer::new(|| {
-        App::new().service(web::scope("/api").service(web::scope("/v1").service(
-            web::scope("/channel").service(web::scope("/twitch").service(
-                web::scope("/{name}").service(web::resource("").to(get_channel_by_twitch_id)),
-            )),
-        )))
+        App::new().service(
+            web::scope("/api").service(
+                web::scope("/v1").service(
+                    web::scope("/channel").service(
+                        web::scope("/twitch").service(
+                            web::scope("/{name}")
+                                .service(web::resource("").to(get_channel_by_twitch_id))
+                                .service(
+                                    web::resource("/emotes").to(get_channel_emotes_by_twitch_id),
+                                ),
+                        ),
+                    ),
+                ),
+            ),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- api/src/routes/channel.rs
+++ api/src/routes/channel.rs
@@ -1,7 +1,9 @@
 use actix_web::{get, http::StatusCode, web, Responder};
 use common::{
-    establish_connection, models::Channel, schema::channels::dsl as ch, ExpressionMethods,
-    QueryDsl, RunQueryDsl,
+    establish_connection,
+    models::{Channel, ChannelEmote},
+    schema::channels::dsl as ch,
+    BelongingToDsl, ExpressionMethods, QueryDsl, RunQueryDsl,
 };
 
 use super::models::Response;
@@ -33,6 +35,52 @@ pub async fn get_channel_by_twitch_id(name: web::Path<i32>) -> impl Responder {
             StatusCode::NOT_FOUND,
         ),
     };
+
+    (web::Json(response.0), response.1)
+}
+
+pub async fn get_channel_emotes_by_twitch_id(name: web::Path<i32>) -> impl Responder {
+    let conn = &mut establish_connection();
+
+    let channel: Channel = match ch::channels
+        .filter(ch::alias_id.eq(&*name))
+        .get_result(conn)
+    {
+        Ok(v) => v,
+        Err(_) => {
+            return (
+                web::Json(Response {
+                    status_code: 404,
+                    message: Some(format!(
+                        "Twitch user ID {} doesn't exist in my database",
+                        name
+                    )),
+                    data: None::<Vec<ChannelEmote>>,
+                }),
+                StatusCode::NOT_FOUND,
+            );
+        }
+    };
+
+    let response: (Response<Vec<ChannelEmote>>, StatusCode) =
+        match ChannelEmote::belonging_to(&channel).get_results(conn) {
+            Ok(v) => (
+                Response {
+                    status_code: 200,
+                    message: None,
+                    data: Some(v),
+                },
+                StatusCode::OK,
+            ),
+            Err(_) => (
+                Response {
+                    status_code: 404,
+                    message: Some(format!("No channel emotes for Twitch user ID {}", name)),
+                    data: None,
+                },
+                StatusCode::NOT_FOUND,
+            ),
+        };
 
     (web::Json(response.0), response.1)
 }
