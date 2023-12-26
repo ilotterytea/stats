4cadaf72481cd93e154c2d3678e06d63ce9cde98
ilotterytea <ilotterytea@proton.me>
2023-12-26 19:35:33 +0600

    feat: api powered by actix_web

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use std::io::Result;
+
+use actix_web::{App, HttpServer};
+
+mod routes;
+
+#[actix_web::main]
+async fn main() -> Result<()> {
     println!("Hello, world!");
+
+    HttpServer::new(|| App::new().service(crate::routes::channel::index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
