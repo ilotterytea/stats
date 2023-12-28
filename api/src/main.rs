f1846f46744836e4e4b1ef86b0c7286801bc6ef0
ilotterytea <ilotterytea@proton.me>
2023-12-28 23:04:40 +0600

    feat: shared twitch data

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,19 +1,50 @@
-use std::io::Result;
+use std::{env, io::Result};
 
 use actix_web::{web, App, HttpServer};
+use twitch_api::{
+    twitch_oauth2::{AccessToken, UserToken},
+    HelixClient,
+};
 
 use crate::routes::channel::*;
 
 mod routes;
 
+pub struct SharedTwitchData {
+    pub token: UserToken,
+    pub api_client: HelixClient<'static, reqwest::Client>,
+}
+
 #[actix_web::main]
 async fn main() -> Result<()> {
     dotenvy::dotenv().expect("Failed to load .env");
 
+    let reqwest_client = reqwest::Client::default();
+
+    let helix_token = match UserToken::from_token(
+        &reqwest_client,
+        AccessToken::new(
+            env::var("BOT_ACCESS_TOKEN")
+                .unwrap_or_else(|_| panic!("No BOT_ACCESS_TOKEN value specified in .env file!")),
+        ),
+    )
+    .await
+    {
+        Ok(token) => token,
+        Err(e) => panic!("Failed to construct user token: {}", e),
+    };
+
+    let helix_client = HelixClient::with_client(reqwest_client.clone());
+
+    let shared_twitch_data = web::Data::new(SharedTwitchData {
+        token: helix_token,
+        api_client: helix_client,
+    });
+
     println!("Hello, world!");
 
-    HttpServer::new(|| {
-        App::new().service(
+    HttpServer::new(move || {
+        App::new().app_data(shared_twitch_data.clone()).service(
             web::scope("/api").service(
                 web::scope("/v1").service(
                     web::scope("/channel").service(
